11503098fd6a9a8447223fd323e5903c4e4bdadb
Rashanand <tech.rashanand@gmail.com>
2019-06-04 23:22:51 +0530

    ProTeam maven-boot application

---
+++ ProTeam/src/main/java/com/pts/ProTeamApplication.java
@@ -0,0 +1,14 @@
+package com.pts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class ProTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProTeamApplication.class, args);
+	}
+
+}
